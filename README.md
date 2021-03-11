c8fa8d069e30db724c3033e1f2b884d80751fddd
Rushabh Shah <rushabh.shah@harness.io>
2021-03-10 21:59:23 -0800

    Updating the java sample program and readme file

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 This is a sample app demonstrating java sdk integration with CF 
 
+##To use this application, follow the steps as below 
+
+1) Create a project in Harness with Feature-flags module enabled
+2) Create an environment within your project
+3) Create a server-side sdk key in your environment **COPY the value from the Admin Console to your clipboard since this value will only be displayed once**
+4) Create a boolean feature-flag in the admin console
+5) Import the maven project in an IDE such as [IntelliJ](https://www.jetbrains.com/idea/)
+6) Replace the values for SDK Key and feature-flag identifier in the example program from step 3 and 4
+7) Run the program from the IDE
+
 We are using the java sdk jar as dependency for this sample program
-``` <dependency>
-               <groupId>io.harness.cf</groupId>
-               <artifactId>cf-client-sdk-java</artifactId>
-               <version>1.0.0</version>
-           </dependency>
+``` 
+            <dependency>
+                <groupId>io.harness</groupId>
+                <artifactId>ff-java-server-sdk</artifactId>
+                <version>0.0.1</version>
+            </dependency>
 ```
 
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,20 +1,40 @@
 import io.harness.cf.client.api.CfClient;
 import io.harness.cf.client.api.Config;
 import io.harness.cf.client.dto.Target;
-
 import java.util.HashMap;
 
+/**
+ * This is a sample program that demonstrates a simple integration of the
+ * ff-java-server-sdk with Harness Feature Flags
+ */
 public class HelloWorld {
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     try {
-      String apiKey = "17e7c975-06c1-4f09-98f4-2c33d6c82974";
+      /**
+       * Put the API Key here from your environment
+       */
+      String apiKey = "a2a03d94-b37d-49b8-8566-1c29b86ab255";
       CfClient cfClient =
-          new CfClient(apiKey, Config.builder().baseUrl("https://config.feature-flags.uat.harness.io/api/1.0").build());
-      Target target =
-          Target.builder().name("User1").custom(new HashMap<String, Object>()).identifier("user1@example.com").build();
+          new CfClient(apiKey, Config.builder().anayticsEnabled(false).build());
+      /**
+       * Define you target on which you would like to evaluate the featureFlag
+       */
+      Target target = Target.builder()
+                          .name("User1")
+                          .attributes(new HashMap<String, Object>())
+                          .identifier("user1@example.com")
+                          .build();
       while (true) {
+        /**
+         * Sleep for sometime before printing the value of the flag
+         */
         Thread.sleep(2000);
-        boolean result = cfClient.boolVariation("flag", target, false);
+        /**
+         * This is a sample boolean flag. You can replace the flag value with
+         * the identifier of your feature flag
+         */
+        boolean result =
+            cfClient.boolVariation("sample_boolean_flag", target, false);
         System.out.println("Boolean variation is " + result);
       }
     } catch (Exception e) {
